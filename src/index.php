7408d99d6b85c006143856c72f0345ca4b06eecc
iwafuchi <rta375@gmail.com>
2022-07-10 21:37:59 +0900

    first commit

--- src/index.php
+++ src/index.php
@@ -1,1 +1,11 @@
 <?php
+
+require './vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// .envファイルで定義したGREETINGを変数に代入
+$greeting = $_ENV['GREETING'];
+
+print ($greeting) . PHP_EOL;
