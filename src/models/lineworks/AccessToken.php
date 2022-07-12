3240189c1dba57b288ad540e72e15d440036505b
iwafuchi <rta375@gmail.com>
2022-07-12 17:31:42 +0900

    update AccessToken.php

--- src/models/lineworks/AccessToken.php
+++ src/models/lineworks/AccessToken.php
@@ -20,10 +20,36 @@ class AccessToken {
     private $currentTime;
     public function getAccessToken() {
         $this->currentTime = time();
-        $this->generateSignature();
+        print_r($this->requestAccessToken());
         return $this->currentTime;
     }
     private function requestAccessToken(): array {
+        $signature = $this->generateSignature();
+        $option = [
+            "headers" => [
+                "Content-Type" => "application/x-www-form-urlencoded"
+            ],
+            "form_params" => [
+                "assertion" => $signature,
+                "grant_type" => "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                "client_id" => Env::get("ClientID"),
+                "client_secret" => Env::get("ClientSecret"),
+                "scope" => "bot,user.read"
+            ],
+            "http_errors" => false,
+            "verify" => false
+        ];
+        try {
+            $client = new Client();
+            $accessTokenUrl = "https://auth.worksmobile.com/oauth2/v2.0/token";
+            $response = $client->request("POST", $accessTokenUrl, $option);
+            if ($response->getStatusCode() === 200) {
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $result;
+            }
+        } catch (ClientException $e) {
+            return $e;
+        }
         return array();
     }
     private function generateSignature(): string {
