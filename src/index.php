3c0dc83e394b9edcba05ad733dc8c374875b1aae
iwafuchi <rta375@gmail.com>
2022-07-15 14:37:52 +0900

    add message edit mode

--- src/index.php
+++ src/index.php
@@ -6,13 +6,45 @@ require_once('../vendor/autoload.php');
 
 use Models\lineworks\Notice;
 
-session_start();
-session_regenerate_id();
 
-if (!isset($_SESSION["count"])) {
-    $_SESSION["count"] = 0;
-}
-$_SESSION["count"] = $_SESSION["count"] + 1;
+?>
 
-$notice = new Notice();
-echo $notice->notifyMessage("sho@iwafuchi", (string)$_SESSION["count"]);
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lineworks Notice to Message Bot</title>
+</head>
+
+<body>
+    <h1>Lineworks Notice to Message Bot</h1>
+    <form method="post">
+        <textarea name="message" rows="5" cols="40"></textarea>
+        <p>
+            <button type="submit" name="fetch" value>送信</button>
+        </p>
+    </form>
+    <?php
+    if (isset($_POST["fetch"])) {
+        session_start();
+        session_regenerate_id();
+
+        if (!isset($_SESSION["count"])) {
+            $_SESSION["count"] = 0;
+        }
+        $_SESSION["count"] = $_SESSION["count"] + 1;
+        $notice = new Notice();
+        if (isset($_POST["message"]) && $_POST["message"]) {
+            echo $notice->notifyMessage("sho@iwafuchi", $_POST["message"]);
+        }
+        if (isset($_POST["fetch"])) {
+            unset($_POST["fetch"]);
+        }
+    }
+    ?>
+</body>
+
+</html>
